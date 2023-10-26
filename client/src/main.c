3a4c79f8fd01458034955c6f7aa5646ce9298fd2
cp <ctpanning@gmail.com>
2023-10-25 23:11:04 -0700

    added headers and main function

--- client/src/main.c
+++ client/src/main.c
@@ -4,8 +4,12 @@
 
   Description:
 */
+#include <arpa/inet.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include "sockets.h"
 
 
 
@@ -17,8 +21,6 @@
    The main driver function for the client side of client/server data transmission.
 */
 int main(void)
-{
-
-  
+{  
   return 0;
 } // end of "main"
