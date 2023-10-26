2dc6133744f26de8511301b7d862a901168467c4
cp <ctpanning@gmail.com>
2023-10-26 00:13:21 -0700

    now sends message to server and waits on a response

--- client/src/main.c
+++ client/src/main.c
@@ -21,6 +21,29 @@
    The main driver function for the client side of client/server data transmission.
 */
 int main(void)
-{  
+{
+
+  int clientSocket;
+  struct sockaddr_in serverAddress;
+
+  // create client socket
+  clientSocket = createStreamSocketWrapper();
+
+  // define the IPv4 server address
+  initSocketAddressWrapper(&serverAddress,
+			   inet_addr("127.0.0.1")); // loopback
+
+  // attempt connection to server
+  connectWrapper(&clientSocket, &serverAddress);
+
+  const char* message = "from client";
+  send(clientSocket, message, strlen(message), 0);
+
+  char response[256];
+  int bytesRead = recv(clientSocket, response, sizeof(response), 0);
+
+  response[bytesRead] = 0;
+  fprintf(stdout, "Server Response: %s\n", response);
+  
   return 0;
 } // end of "main"
