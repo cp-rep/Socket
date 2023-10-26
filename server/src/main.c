234a17b8798bbbbc39dbe6c12c43ced6a961491a
cp <ctpanning@gmail.com>
2023-10-25 20:34:11 -0700

    basic main driver functions and layout started

---
+++ server/src/main.c
@@ -0,0 +1,24 @@
+/*
+  File:
+   main.c
+
+  Description:
+
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+/*
+  Function:
+   main
+
+  Description:
+   The main driver function for the server side of client/server data transmission.
+ */
+int main(void)
+{
+
+  return 0;
+} // end of "main"
