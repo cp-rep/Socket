837a82259e755f8843b224b99da160fa24b5a3a6
cp <ctpanning@gmail.com>
2023-10-27 16:44:38 -0700

    main now parses command line arguments to determine if the client wants to send a message or a file to the defined server address

--- client/src/main.c
+++ client/src/main.c
@@ -3,6 +3,10 @@
    main.c
 
   Description:
+   The main driver file for the client side of client/server data transmission.
+   The client attempts to connect with the server to send a buffer of data containing
+   a message, and then awaits a response.  Or, the client attempts to send a file byte
+   by byte to the server.
 */
 #include <arpa/inet.h>
 #include <stdio.h>
@@ -10,6 +14,10 @@
 #include <string.h>
 #include <sys/socket.h>
 #include "sockets.h"
+
+#define _SERVERIP "127.0.0.1"
+#define _SERVERPORT 8000
+#define _PATTERN "Pattern is: ./client [-m, -f] where -m is for message and 'f' for file\n"
 
 
 
@@ -20,30 +28,55 @@
   Description:
    The main driver function for the client side of client/server data transmission.
 */
-int main(void)
+int main(int argc, char** argv)
 {
+  if(argc == 1)
+    {
+      fprintf(stderr, "Not enough arguments.\n");
+      fprintf(stderr, _PATTERN);
+      exit(EXIT_FAILURE);
+    }
+  else if (argc > 2)
+    {
+      fprintf(stderr, "Too many arguments.\n");
+      fprintf(stderr, _PATTERN);
+      exit(EXIT_FAILURE);
+    }
 
+  // connection vars
   int clientSocket;
   struct sockaddr_in serverAddress;
+  struct sockaddr_in clientAddress;
+  socklen_t addr_size;
 
-  // create client socket
+  // define client/server connection
   clientSocket = createStreamSocketWrapper();
+  initSocketAddressWrapper(&serverAddress, inet_addr(_SERVERIP), _SERVERPORT);
 
-  // define the IPv4 server address
-  initSocketAddressWrapper(&serverAddress,
-			   inet_addr("127.0.0.1")); // loopback
-
-  // attempt connection to server
+  // connect to server
   connectWrapper(&clientSocket, &serverAddress);
+  
+  if(strcmp(argv[1], "-m") == 0)
+    {
+      // attempt to send message to server
+      const char* message = "from client";
+      send(clientSocket, message, strlen(message), 0);
 
-  const char* message = "from client";
-  send(clientSocket, message, strlen(message), 0);
+      // wait for response
+      char response[256];
+      int bytesRead = recv(clientSocket, response, sizeof(response), 0);
 
-  char response[256];
-  int bytesRead = recv(clientSocket, response, sizeof(response), 0);
-
-  response[bytesRead] = 0;
-  fprintf(stdout, "Server Response: %s\n", response);
+      //print response
+      response[bytesRead] = 0;
+      fprintf(stdout, "Server Response: %s\n", response);
+    }
+  if(strcmp(argv[1], "-f") == 0)
+    {
+      
+    }
+  
+  // clean up
+  close(clientSocket);
   
   return 0;
 } // end of "main"
