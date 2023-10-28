0a92d492fdaf207a45407d8114776cac414d2fb2
cp <ctpanning@gmail.com>
2023-10-27 21:43:26 -0700

    added option to receive a file from a client address

--- server/src/main.c
+++ server/src/main.c
@@ -45,51 +45,110 @@ int main(int argc, char** argv)
       exit(EXIT_FAILURE);
     }
 
+  int serverSocket;
+  int clientSocket;
+  struct sockaddr_in serverAddress;
+  struct sockaddr_in clientAddress;
+  socklen_t clientAddressLen;
+
+  // creat the socket
+  serverSocket = createStreamSocketWrapper();
+
+  // define server address IPv4 data
+  initSocketAddressWrapper(&serverAddress,
+			   INADDR_ANY,
+			   _SERVERPORT);
+  
+  // bind the socket to server address
+  bindWrapper(&serverSocket,
+	      &serverAddress);
+
+  // set socket into listening mode
+  listenWrapper(&serverSocket,
+		    _QUEUELEN);
+  
+  fprintf(stdout, "Servering listening on port %d...\n", _SERVERPORT);
+
+  // accept and handle incoming connections
+  acceptWrapper(&clientSocket,
+		&serverSocket,
+		&clientAddress);
+  
   if(strcmp(argv[1], "-m") == 0)
     {
-  
-      int serverSocket = 0;
-      struct sockaddr_in serverAddress;
-  
-      // creat the socket
-      serverSocket = createStreamSocketWrapper();
+      const char* toClient = "You have successfully connected to the server.";
+      int returnVal;
+      char buff[256];
+      int bytesRead;
 
-      // define server address IPv4 data
-      initSocketAddressWrapper(&serverAddress,
-			       INADDR_ANY,
-			       _SERVERPORT);
-  
-      // bind the socket to server address
-      bindWrapper(&serverSocket, &serverAddress);
-  
-      // set socket into listening mode
-      listenWrapper(&serverSocket,
-		    _QUEUELEN);
-
-      fprintf(stdout, "Servering listening on port %d...\n", _SERVERPORT);
-  
-      while(_TRUE)
-	{
-	  // handle incoming connections
-	  struct sockaddr_in clientAddress;
-	  socklen_t clientAddressLen = sizeof(clientAddress);
+      // attempt to send message to client      
+      returnVal = send(clientSocket, toClient, strlen(toClient), 0);
       
-	  int clientSocket = accept(serverSocket,
-				    (struct sockaddr*)&clientAddress,
-				    &clientAddressLen);
-	  if(clientSocket == -1)
-	    {
-	      perror("Error accepting connection.\n");
-	      continue;
-	    }
+      // attempt to receive message from client
+      bytesRead = recvWrapper(&clientSocket,
+			      &serverSocket,
+			      buff,
+			      sizeof(buff),
+			      0);
 
-	  const char* response = "Hello.";
-	  send(clientSocket, response, strlen(response), 0);
-	}
-    }  
+      // null terminate the received message
+      buff[bytesRead] = 0;
+
+      // print received message to stdout
+      fprintf(stdout, "From client:  %s\n", buff);
+    }
   else if(strcmp(argv[1], "-f") == 0)
     {
+      char buff[1];
+      FILE* saveFile;
+      int bytesRead;
+      char* fileName = "receivedFile.txt";
+      char* mode = "w+";
+      int writeVal;
 
+      // open file for writing
+      fopenServerWrapper(&clientSocket,
+			 &serverSocket,
+			 &saveFile,
+			 fileName,
+			 mode);
+
+      // loop and receive data from client
+      while(_TRUE)
+	{
+	  // receive data
+	  bytesRead = recv(clientSocket, buff, sizeof(buff), 0);
+
+	  // check for error
+	  if(bytesRead < 0)
+	    {
+	      perror("recv() failed");
+	      break;
+	    }
+	  // test for EOF
+	  else if(bytesRead == 0)
+	    {
+	      break;
+	    }
+
+	  // write data to file
+	  writeVal = fwrite(buff,
+			    1,
+			    bytesRead,
+			    saveFile);
+	  
+	  // test for bad write
+	  if(writeVal < 0)
+	    {
+	      perror("fwrite() failed");
+	      break;
+	    }
+	}
+
+      // clean up
+      fclose(saveFile);
+      close(clientSocket);
+      close(serverSocket);
     }
   
   return 0;
